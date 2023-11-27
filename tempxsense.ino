9e5c565ef189f2ff15de53e54ac4467a282d3db3
Abdul Sahib Nizer <88323547+xx420@users.noreply.github.com>
2023-11-27 14:27:55 +0200

    Updatedtempxsense.ino

--- tempxsense.ino
+++ tempxsense.ino
@@ -1,178 +1,139 @@
-//MAX30100 ESP32 WebServer
 #include <WiFi.h>
+#include <WiFiClient.h>
 #include <WebServer.h>
-#include <Wire.h>
-#include "MAX30100_PulseOximeter.h"
+#include <ESPmDNS.h>
+#include <DHT.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define REPORTING_PERIOD_MS     1000
-
-float BPM, SpO2;
-
-/*Put your SSID & Password*/
-const char* ssid = "KHAN";  // Enter SSID here
-const char* password = "12345678";  //Enter Password here
-
-PulseOximeter pox;
-uint32_t tsLastReport = 0;
+const char *ssid = "KHAN";
+const char *password = "12345678";
 
 WebServer server(80);
+DHT dht(26, DHT22);
+OneWire oneWire(5);  // DS18B20 connected to Pin D4
+DallasTemperature sensors(&oneWire);
 
-void onBeatDetected()
-{
-  Serial.println("Beat Detected!");
+float dhtTemperature;
+float dhtHumidity;
+float ds18b20Temperature;
+
+void handleRoot() {
+  char msg[1500];
+
+  // Check if DS18B20 temperature is above 27 degrees Celsius
+  bool urgentAttention = ds18b20Temperature > 27.0;
+
+  snprintf(msg, 1500,
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='4'/>\
+    <meta name='viewport' content='width=device-width, initial-scale=1'>\
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>\
+    <title>ESP32 Sensor Readings</title>\
+    <style>\
+    html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center;}\
+    h2 { font-size: 3.0rem; }\
+    p { font-size: 3.0rem; }\
+    .units { font-size: 1.2rem; }\
+    .dht-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px;}\
+    </style>\
+    <script>\
+      function checkTemperature() {\
+        if (%s) {\
+          alert('Seek Medical Attention Urgently!');\
+        }\
+      }\
+      window.onload = checkTemperature;\
+    </script>\
+  </head>\
+  <body>\
+      <h2>ESP32 Sensor Readings!</h2>\
+      <p>\
+        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>DHT22 Temperature</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&deg;C</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-tint' style='color:#00add6;'></i>\
+        <span class='dht-labels'>DHT22 Humidity</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&percnt;</sup>\
+      </p>\
+      <p>\
+        <i class='fas fa-thermometer-half' style='color:#ca3517;'></i>\
+        <span class='dht-labels'>DS18B20 Temperature</span>\
+        <span>%.2f</span>\
+        <sup class='units'>&deg;C</sup>\
+      </p>\
+  </body>\
+</html>",
+           urgentAttention ? "true" : "false", dhtTemperature, dhtHumidity, ds18b20Temperature
+          );
+  server.send(200, "text/html", msg);
 }
 
-void setup() {
+void setup(void) {
   Serial.begin(115200);
-  pinMode(19, OUTPUT);
-  delay(100);
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-
-  //connect to your local wi-fi network
+  dht.begin();
+  sensors.begin();
+  
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  Serial.println("");
 
-  //check wi-fi is connected to wi-fi network
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
-  
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+  server.on("/", handleRoot);
 
   server.begin();
   Serial.println("HTTP server started");
-  
-  Serial.print("Initializing pulse oximeter..");
-
-  if (!pox.begin()) {
-    Serial.println("FAILED");
-    for (;;);
-  } else {
-    Serial.println("SUCCESS");
-
-    pox.setOnBeatDetectedCallback(onBeatDetected);
-  }
-
-  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
-
-  // Register a callback for the beat detection
-
 }
-void loop() {
+
+void loop(void) {
+  dhtTemperature = readDHTTemperature();
+  dhtHumidity = readDHTHumidity();
+
+  sensors.requestTemperatures();
+  ds18b20Temperature = sensors.getTempCByIndex(0);
+
   server.handleClient();
-  pox.update();
-  BPM = pox.getHeartRate();
-  SpO2 = pox.getSpO2();
+  delay(2000); // Allow the CPU to switch to other tasks
+}
 
-  if (millis() - tsLastReport > REPORTING_PERIOD_MS)
-  {
-
-    Serial.print("BPM: ");
-    Serial.println(BPM);
-
-    Serial.print("SpO2: ");
-    Serial.print(SpO2);
-    Serial.println("%");
-
-    Serial.println("*********************************");
-    Serial.println();
-
-    tsLastReport = millis();
+float readDHTTemperature() {
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
   }
-
+  else {
+    Serial.println("DHT Temperature: " + String(t));
+    return t;
+  }
 }
 
-void handle_OnConnect() {
-
-  server.send(200, "text/html", SendHTML(BPM, SpO2));
-}
-
-void handle_NotFound() {
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float BPM, float SpO2) {
-  String ptr = "<!DOCTYPE html>";
-  ptr += "<html>";
-  ptr += "<head>";
-  ptr += "<title>Pulse Oximeter ESP32 WebServer</title>";
-  ptr += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
-  ptr += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'>";
-  ptr += "<link rel='stylesheet' type='text/css' href='styles.css'>";
-  ptr += "<style>";
-  ptr += "body { background-color: #fff; font-family: sans-serif; color: #333333; font: 14px Helvetica, sans-serif box-sizing: border-box;}";
-  ptr += "#page { margin: 20px; background-color: #fff;}";
-  ptr += ".container { height: inherit; padding-bottom: 20px;}";
-  ptr += ".header { padding: 20px;}";
-  ptr += ".header h1 { padding-bottom: 0.3em; color: #008080; font-size: 45px; font-weight: bold; font-family: Garmond, 'sans-serif'; text-align: center;}";
-  ptr += "h2 { padding-bottom: 0.2em; border-bottom: 1px solid #eee; margin: 2px; text-align: left;}";
-  ptr += ".header h3 { font-weight: bold; font-family: Arial, 'sans-serif'; font-size: 17px; color: #b6b6b6; text-align: center;}";
-  ptr += ".box-full { padding: 20px; border 1px solid #ddd; border-radius: 1em 1em 1em 1em; box-shadow: 1px 7px 7px 1px rgba(0,0,0,0.4); background: #fff; margin: 20px; width: 300px;}";
-  ptr += "@media (max-width: 494px) { #page { width: inherit; margin: 5px auto; } #content { padding: 1px;} .box-full { margin: 8px 8px 12px 8px; padding: 10px; width: inherit;; float: none; } }";
-  ptr += "@media (min-width: 494px) and (max-width: 980px) { #page { width: 465px; margin 0 auto; } .box-full { width: 380px; } }";
-  ptr += "@media (min-width: 980px) { #page { width: 930px; margin: auto; } }";
-  ptr += ".sensor { margin: 12px 0px; font-size: 2.5rem;}";
-  ptr += ".sensor-labels { font-size: 1rem; vertical-align: middle; padding-bottom: 15px;}";
-  ptr += ".units { font-size: 1.2rem;}";
-  ptr += "hr { height: 1px; color: #eee; background-color: #eee; border: none;}";
-  ptr += "</style>";
-
-  //Ajax Code Start
-  ptr += "<script>\n";
-  ptr += "setInterval(loadDoc,1000);\n";
-  ptr += "function loadDoc() {\n";
-  ptr += "var xhttp = new XMLHttpRequest();\n";
-  ptr += "xhttp.onreadystatechange = function() {\n";
-  ptr += "if (this.readyState == 4 && this.status == 200) {\n";
-  ptr += "document.body.innerHTML =this.responseText}\n";
-  ptr += "};\n";
-  ptr += "xhttp.open(\"GET\", \"/\", true);\n";
-  ptr += "xhttp.send();\n";
-  ptr += "}\n";
-  ptr += "</script>\n";
-  //Ajax Code END
-
-  ptr += "</head>";
-  ptr += "<body>";
-  ptr += "<div id='page'>";
-  ptr += "<div class='header'>";
-  ptr += "<h1>MAX30100 ESP32 WebServer</h1>";
-  ptr += "<h3><a href='The readings are as follows:'>The readings are as follows:</a></h3>";
-  ptr += "</div>";
-  ptr += "<div id='content' align='center'>";
-  ptr += "<div class='box-full' align='left'>";
-  ptr += "<h2>Sensor Readings</h2>";
-  ptr += "<div class='sensors-container'>";
-
-
-  //For Heart Rate
-  ptr += "<p class='sensor'>";
-  ptr += "<i class='fas fa-heartbeat' style='color:#cc3300'></i>";
-  ptr += "<span class='sensor-labels'> Heart Rate </span>";
-  ptr += (int)BPM;
-  ptr += "<sup class='units'>BPM</sup>";
-  ptr += "</p>";
-  ptr += "<hr>";
-
-  //For Sp02
-  ptr += "<p class='sensor'>";
-  ptr += "<i class='fas fa-burn' style='color:#f7347a'></i>";
-  ptr += "<span class='sensor-labels'> Sp02 </span>";
-  ptr += (int)SpO2;
-  ptr += "<sup class='units'>%</sup>";
-  ptr += "</p>";
-
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</div>";
-  ptr += "</body>";
-  ptr += "</html>";
-  return ptr;
+float readDHTHumidity() {
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else {
+    Serial.println("DHT Humidity: " + String(h));
+    return h;
+  }
 }
